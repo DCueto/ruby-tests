7e92160a9670c463e55b13aa53963ed3d2c41bc3
DCueto <dcueto14.info@gmail.com>
2015-05-28 23:17:30 +0200

    CodeHero - Ruby desde Cero: Instalación y Configuración

---
+++ CodeHero/pruebaCodeHero.rb
@@ -0,0 +1,17 @@
+# Imprimir cadena de texto
+puts "Bienvenidos a CodeHero"
+
+# Suma simple
+puts 2 + 3
+
+# Multiplcación
+puts 3 * 6
+
+# Elevar al cuadrado
+puts 3 ** 2
+
+# Imprime una cadera al reves
+puts 'CodeHero'.reverse
+
+# Imprime todo en mayúscula
+puts "codeHero".upcase
